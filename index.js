6bbffab7414e2fc3a247dd30016ba7f4b725fb16
wangdan <821451612@qq.com>
2017-09-25 09:50:48 +0800

    'pc'

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by admin on 2017/9/23.
+ */
+var express = require('express')
+var app  = express()
+app.use(express.static('public'))
+
+//引入数据
+var data=require()
+
+app.listen(3000)
